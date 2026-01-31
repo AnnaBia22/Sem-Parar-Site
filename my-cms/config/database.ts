290352a8ed64f63da40995e6c287b607aa5d2e3c
AnnaBia22 <abgtravascio@gmail.com>
2026-01-31 19:16:00 -0300

    Sinc strapi

--- my-cms/config/database.ts
+++ my-cms/config/database.ts
@@ -24,19 +24,15 @@ export default ({ env }) => {
     },
     postgres: {
       connection: {
+        // Se existir DATABASE_URL (no Render tem), ele usa ela. Se não, usa os campos soltos.
         connectionString: env('DATABASE_URL'),
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+        ssl: env.bool('DATABASE_SSL', false) || {
+          rejectUnauthorized: false, // Necessário para o Render aceitar a conexão SSL
         },
         schema: env('DATABASE_SCHEMA', 'public'),
       },
