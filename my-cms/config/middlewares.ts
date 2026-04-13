ee81473ce043226e9a0cbb0b8eebe7b3301d274d
AnnaBia22 <abgtravascio@gmail.com>
2026-04-13 00:48:59 -0300

    refinando

--- my-cms/config/middlewares.ts
+++ my-cms/config/middlewares.ts
@@ -2,20 +2,40 @@ export default [
   'strapi::logger',
   'strapi::errors',
   {
-  name: 'strapi::security',
-  config: {
-    contentSecurityPolicy: {
-      useDefaults: true,
-      directives: {
-        'connect-src': ["'self'", 'https:'],
-        'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-        'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-        upgradeInsecureRequests: null,
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'res.cloudinary.com', // Cloudinary
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'res.cloudinary.com', // Cloudinary
+          ],
+          upgradeInsecureRequests: null,
+        },
       },
     },
   },
-},
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['*'], // No início, use '*' para testar se os dados aparecem. Depois mude para o link da Vercel.
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
